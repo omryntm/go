a96aa77e8b7d0d4fa836aa6c856e756197244695
omer <omer@Omer-iMac.local>
2023-09-27 11:51:51 +0300

    workshop4

--- loops/workshop4.go
+++ loops/workshop4.go
@@ -12,14 +12,14 @@ func Demo6() {
 	toplam1 := 0
 	toplam2 := 0
 
-	for i := 0; i < sayi1; i++ {
+	for i := 1; i < sayi1; i++ {
 		if sayi1%i == 0 {
 			toplam1 = toplam1 + i
 		}
 
 	}
 
-	for i := 0; i < sayi2; i++ {
+	for i := 1; i < sayi2; i++ {
 		if sayi2%i == 0 {
 			toplam2 = toplam2 + i
 		}
